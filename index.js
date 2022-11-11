13bc923d866dafccec30ec4d1abe010c666ac6c0
bongiu <peppedeka@gmail.com>
2022-11-11 15:25:04 +0100

    fix host

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/search', (req, resMain) => {
     const geoHubId = req.query.id || '3';
     const search = req.query.query != undefined ? req.query.query : '' || '';
     //var hostName = `https://elastic.webmapp.it/geohub_app_${geoHubId}/_search/`;
-    var hostName = `localhost:9200/geohub_app_${geoHubId}/_search/`;
+    var hostName = `127.0.0.1:9200/geohub_app_${geoHubId}/_search/`;
     var auth = 'Basic Zm9yZ2U6MWIwVlVKeFJGeGVPdXBralBlaWU='
     const method = 'POST'
     const body = {
