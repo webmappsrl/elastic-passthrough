0c3383a0669f95ab7e2f3d71378da2cb20898694
bongiu <peppedeka@gmail.com>
2023-01-12 14:58:04 +0100

    add filter by layer

--- index.js
+++ index.js
@@ -12,21 +12,46 @@ app.get('/', function (req, res) {
 app.get('/search', (req, resMain) => {
     const geoHubId = req.query.id || '3';
     const search = req.query.query != undefined ? req.query.query : '' || '';
+    const layer = req.query.layer || null;
     // var hostName = `https://elastic.webmapp.it/geohub_app_${geoHubId}/_search/`;
     var hostName = `http://127.0.0.1:3000/geohub_app_${geoHubId}/_search/`;
     var auth = 'Basic Zm9yZ2U6MWIwVlVKeFJGeGVPdXBralBlaWU='
     const method = 'POST'
+    let size = 20000;
+
+    let query ={
+        query_string: {
+            fields: ['name','from','to','ref'],
+            query: `*${search}*`,
+        },
+    }
+    if (layer != null) {
+        query = {
+            bool: {
+                must: [
+                    {
+                        query_string: {
+                            fields: ['name', 'from', 'to', 'ref'],
+                            query: `*${search}*`,
+                        }
+                    },
+                    {
+                        terms: {
+                            layers: [layer],
+                        }
+                    }
+                ]
+            }
+
+        }
+        size= 200;
+    }
     const body = {
         _source: {
             excludes: ['geometry'],
         },
-        query: {
-            query_string: {
-                fields: ['name','from','to','ref'],
-                query: `*${search}*`,
-            },
-        },
-        size:20000
+        query,
+        size
     }
     request({
         url: hostName,
