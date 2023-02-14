2e5d2bf53ece8b05a928daf73b34dafe263e5b2b
bongiu <peppedeka@gmail.com>
2023-02-14 08:53:24 +0100

    add track endpoint

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const app = require('express')()
 const request = require('request')
 const cors = require('cors')
 app.use(cors())
-
+// const BASE_URL = 'https://elastic.webmapp.it'
+const BASE_URL = 'http://127.0.0.1:9200';
 const  PORT = process.env.PORT || 3000;
+const method = 'POST'
+const auth = 'Basic Zm9yZ2U6MWIwVlVKeFJGeGVPdXBralBlaWU='
+
+getHost = (id = 3) => {
+    return    `${BASE_URL}/geohub_app_${id}/_search/`;
+}
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
@@ -13,10 +20,8 @@ app.get('/search', (req, resMain) => {
     const geoHubId = req.query.id || '3';
     const search = req.query.query != undefined ? req.query.query : '' || '';
     const layer = req.query.layer || null;
-    // var hostName = `https://elastic.webmapp.it/geohub_app_${geoHubId}/_search/`;
-    var hostName = `http://127.0.0.1:9200/geohub_app_${geoHubId}/_search/`;
-    var auth = 'Basic Zm9yZ2U6MWIwVlVKeFJGeGVPdXBralBlaWU='
-    const method = 'POST'
+    var hostName = getHost(geoHubId);
+  
     let size = 20000;
 
     let query ={
@@ -42,7 +47,6 @@ app.get('/search', (req, resMain) => {
                     }
                 ]
             }
-
         }
         size= 200;
     }
@@ -66,7 +70,25 @@ app.get('/search', (req, resMain) => {
         resMain.send(body)
     });
 
-
-
 });
+app.get('/track',(req, resMain) => {
+    const geoHubId = req.query.geohub || '3';
+    const trackId =req.query.track || '3';
+    const method = 'GET'
+    var hostName = `${BASE_URL}/geohub_app_${geoHubId}/_doc/${trackId}`;
+
+    request({
+        url: hostName,
+        headers: {
+            "Authorization": auth
+        },
+        method,
+        json: true
+    }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        if(body != null && body._source != null){
+            resMain.send(body._source)
+        }
+    });
+})
 app.listen(PORT);
